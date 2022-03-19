488d9f2d35c1e6d671db0c210c86d9bddf56c3f2
mandar-saiyan87 <66194594+mandar-saiyan87@users.noreply.github.com>
2022-03-19 09:10:18 +0530

    Updated Detail.js

--- disney-plus_frontend/src/components/Detail.js
+++ disney-plus_frontend/src/components/Detail.js
@@ -45,39 +45,3 @@ function Detail() {
 }
 
 export default Detail;
-
-//  <>
-//    <div className="detailContainer">
-//      <div className="background">
-//        <img src="/images/dsmom-bg.jpg" alt="dsmom" />
-//      </div>
-//      <div className="imageTitle">
-//        <img src="/images/dsmom-logo.webp" alt="logo" />
-//      </div>
-//      <div className="controls">
-//        <div className="play">
-//          <img src="/images/play-icon-black.png" alt="play" />
-//          <span>PLAY</span>
-//        </div>
-//        <div className="play trailer">
-//          <img src="/images/play-icon-white.png" alt="trailer" />
-//          <span>TRAILER</span>
-//        </div>
-//        <div className="trailer add">
-//          <span>+</span>
-//        </div>
-//        <div className="trailer group-watch">
-//          <img src="/images/group-icon.png" alt="grp-watch" />
-//        </div>
-//      </div>
-//      <div className="subtitle">
-//        2022 • 2m • Superhero, Action, Adventure, Fantasy • U/A 13+
-//      </div>
-//      <div className="description">
-//        Dr. Stephen Strange casts a forbidden spell that opens the door to the
-//        multiverse, including an alternate version of himself, whose threat to
-//        humanity is too great for the combined forces of Strange, Wong, and Wanda
-//        Maximoff.
-//      </div>
-//    </div>
-//  </>;
