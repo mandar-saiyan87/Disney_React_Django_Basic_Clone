af8f2e2d1ae11b76673f948c5339774a45e3fe8f
mandar-saiyan87 <66194594+mandar-saiyan87@users.noreply.github.com>
2022-03-19 09:05:37 +0530

    indexjs appscss appjs updated

--- disney-plus_frontend/src/App.js
+++ disney-plus_frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/detail" element={<Detail />} />
+          <Route path="/detail/:title" element={<Detail />} />
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
         </Routes>
